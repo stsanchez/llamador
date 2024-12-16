85a4d718396a43d20f8e5356cd2b0d562e7d8442
Sanchez <SSanchez@emergencias.com.ar>
2024-12-16 08:04:04 -0300

    Master a Main

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    
 });
 
 // Carga el valor de CM desde las variables de entorno
